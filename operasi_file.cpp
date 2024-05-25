c3b556c8a413333c37095d082d60e8b2f866f635
Rafif Genadratama <muhammad.rafif.tif423@polban.ac.id>
2024-05-25 09:53:36 +0700

    tambah operasi file dan kursor

--- operasi_file.cpp
+++ operasi_file.cpp
@@ -3,10 +3,10 @@
 void simpan_file(address_pasien trav) {
     FILE *tulis = fopen("file\\data.dat", "wb");
 
-    while (trav != NULL)
-    {
-        /* code */
+    while (trav != NULL) {
         fwrite(trav, sizeof(Pasien), 1, tulis);
         trav = (*trav).p_input;
     }
+
+    fclose(tulis);
 }
