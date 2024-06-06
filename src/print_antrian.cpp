7c32adb8d9f79eae6026a36acb3c4d26d72741c7
Rafif Genadratama <muhammad.rafif.tif423@polban.ac.id>
2024-06-06 22:26:59 +0700

    Nata file kalo mau compile, tinggal run batch dari terminal

--- src/print_antrian.cpp
+++ src/print_antrian.cpp
@@ -6,19 +6,24 @@ void tampilkan_antrian(Pasien *first)
     int jmlh_pasien = 0;
 
     system("cls");
+    banner();
+    printf("\n\n\n");
     if (first == NULL){
+        printf("\t\t\t\t\t\t\t");
         printf("belum terdapat data antrean pasien\n");
     } else {
+        printf("\t\t\t\t\t\t\t");
         printf("Antrian pasien\n");
         while (trav != NULL)
         {   
             jmlh_pasien++;
-            /* code */
+            printf("\t\t\t\t\t\t\t");
             printf("%d. %s dengan nilai vektor %.4f\n", jmlh_pasien, (*trav).nama, (*trav).vektor_total);
             trav = (*trav).p_prioritas;
         }
     }
     printf("\n");
+    printf("\t\t\t\t\t\t\t");
     printf("Tekan apapun untuk kembali.\n");
     getchar();
 }
