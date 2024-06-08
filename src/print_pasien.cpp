43f2ffabe102d534dcb8b975c817ff06ef1f4f87
Rafif Genadratama <muhammad.rafif.tif423@polban.ac.id>
2024-06-08 10:37:03 +0700

    satuin antrian di print_pasien

--- src/print_pasien.cpp
+++ src/print_pasien.cpp
@@ -1,15 +1,4 @@
 #include "pasien.h"
-#define ATAS_KIRI 196
-#define ATAS_KANAN 191
-#define BAWAH_KIRI 192
-#define BAWAH_KANAN 217
-#define HORIZONTAL 196
-#define VERTIKAL 179
-#define T 194
-#define T_PANJANG_KIRI 180
-#define T_PANJANG_ATAS 193
-#define T_PANJANG_KANAN 195
-#define PEREMPATAN 197
 
 void tampilkan_daftar_pasien(Pasien *first)
 {
@@ -25,21 +14,59 @@ void tampilkan_daftar_pasien(Pasien *first)
         printf("Belum terdapat data dalam daftar pasien");
     } else {
         kursor(5, 5);
-        printf("No. Nama                  Alamat                Jenis Kelamin TD Sistole TD Diastole Detak Nadi Detak Jantung Frek. Napas Suhu Badan Elastisitas Tegangan Nadi");
+        printf("No. Nama                  Alamat                JK TD Sistole TD Diastole Detak Nadi Detak Jantung Frek. Napas Suhu Badan Elastisitas Tegangan Nadi");
         kursor(5, 6);
-        printf("--- ---------------------- ---------------------- ------------- ---------- ------------ ---------- ------------- ----------- ---------- ----------- ------------");
+        printf("--- --------------------- --------------------- -- ---------- ----------- ---------- ------------- ----------- ---------- ----------- ------------");
 
         int baris = 7;
         while (trav != NULL) {
             jml_pasien++;
             kursor(5, (short) baris);
-            printf("%2d. %-21s %-21s %c             %3d        %3d         %3d        %3d          %3d        %4.1f   %-11s %-15s", jml_pasien, (*trav).nama, (*trav).alamat, (*trav).jenis_kelamin, (*trav).krit.td_sistole, (*trav).krit.td_diastole, (*trav).krit.detak_nadi, (*trav).krit.detak_jantung, (*trav).krit.frek_napas, (*trav).krit.suhu_badan, ((*trav).krit.elastisitas_pembuluh_nadi == ELASTIS) ? "elastis" : "keras", ((*trav).krit.tegangan_nadi == TIDAK_BERUBAH) ? "tidak berubah" : "berubah-ubah");
+            printf("%2d. %-21s %-21s %c  %10d %11d %10d %13d %11d %10.1f %-11s %-15s", jml_pasien, (*trav).nama, (*trav).alamat, (*trav).jenis_kelamin, (*trav).krit.td_sistole, (*trav).krit.td_diastole, (*trav).krit.detak_nadi, (*trav).krit.detak_jantung, (*trav).krit.frek_napas, (*trav).krit.suhu_badan, ((*trav).krit.elastisitas_pembuluh_nadi == ELASTIS) ? "elastis" : "keras", ((*trav).krit.tegangan_nadi == TIDAK_BERUBAH) ? "tidak berubah" : "berubah-ubah");
             baris++;
             trav = (*trav).p_input;
         }
+        kursor(5, (short) ++baris);
+        printf("JK: Jenis Kelamin");
     }
-    
     printf("\n");
+    printf("\tTekan apapun untuk kembali.\n");
+    getchar();
+}
+
+void tampilkan_antrian(Pasien *first)
+{
+    address_pasien trav = first;
+    int jmlh_pasien = 0;
+
+    system("cls");
+
+
+    kursor(30, 5);
+    printf("ANTRIAN PASIEN");
+    int baris = 10;
+    if (first == NULL) {
+        kursor(20, 8);
+        printf("Belum terdapat data antrean pasien");
+    } else {
+        kursor(5, 8);
+        printf("No. Nama Pasien                          Nilai Vektor");
+        kursor(5, 9);
+        printf("--- -------------------------------------- ------------");
+
+        
+        while (trav != NULL) {
+            jmlh_pasien++;
+            kursor(5, (short)baris);
+            printf("%2d. %-35s %.4f", jmlh_pasien, (*trav).nama, (*trav).vektor_total);
+            baris++;
+            trav = (*trav).p_prioritas;
+        }
+    }
+
+    kursor(20,(short) (baris + 2));
+    printf("\n");
+    printf("\t\t\t\t\t\t\t");
     printf("Tekan apapun untuk kembali.\n");
     getchar();
 }
