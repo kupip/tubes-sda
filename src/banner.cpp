ed9b785b7765a65767d5c11199838ec9746f6a5d
Rafif Genadratama <muhammad.rafif.tif423@polban.ac.id>
2024-09-21 16:08:22 +0700

    betulin banner

--- src/banner.cpp
+++ src/banner.cpp
@@ -1,27 +1,35 @@
 #include "pasien.h"
 
-void banner()
-{
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    int columns;
-  
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-    center_text(".-------.       .-'''-.                  .-''-. ,---------. .---.  .---.    ____    ,---.   .--..-./`)     .-''-.    .---.     ", (unsigned) columns);
-    printf("\n");
-    center_text("|  _ _   \\     / _     \\               .'_ _   \\\\          \\|   |  |_ _|  .'  __ `. |    \\  |  |\\ .-.')  .'_ _   \\   | ,_|     ", (unsigned) columns);
-    printf("\n");
-    center_text("| ( ' )  |    (`' )/`--'              / ( ` )   '`--.  ,---'|   |  ( ' ) /   '  \\  \\|  ,  \\ |  |/ `-' \\ / ( ` )   ',-./  )     ", (unsigned) columns);
-    printf("\n");
-    center_text("|(_ o _) /   (_ o _).                . (_ o _)  |   |   \\   |   '-(_{;}_)|___|  /  ||  |\\_ \\|  | `-'`\"`. (_ o _)  |\\  '_ '`)    ", (unsigned) columns);
-    printf("\n");
-    center_text("| (_,_).' __  (_,_). '.              |  (_,_)___|   :_ _:   |      (_,_)    _.-`   ||  _( )_\\  | .---. |  (_,_)___| > (_)  )    ", (unsigned) columns);
-    printf("\n");
-    center_text("|  |\\ \\  |  |.---.  \\  : _ _         '  \\   .---.   (_I_)   | _ _--.   | .'   _    || (_ o _)  | |   | '  \\   .---.(  .  .-'    ", (unsigned) columns);
-    printf("\n");
-    center_text("|  | \\ `'   /\\    `-'  |( ` )         \\  `-'    /  (_(=)_)  |( ' ) |   | |  _( )_  ||  (_,_)\\  | |   |  \\  `-'    / `-'`-'|___ ", (unsigned) columns);
-    printf("\n");
-    center_text("|  |  \\    /  \\       /(_{;}_)         \\       /    (_I_)   (_{;}_)|   | \\ (_ o _) /|  |    |  | |   |   \\       /   |        \\", (unsigned) columns);
-    printf("\n");
-    center_text("''-'   `'-'    `-...-'  (_,_)           `'-..-'     '---'   '(_,_) '---'  '.(_,_).' '--'    '--' '---'    `'-..-'    `--------`", (unsigned) columns);
+void banner() {
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  int columns;
+  short kolom, baris;
+
+  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+  columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+  kolom = (short)((columns / 2) - 56);
+  kursor(kolom, baris);
+  printf(".-------.       .-'''-.                  .-''-. ,---------. .---.  "
+         ".---.    ____    ,---.   .--..-./`)     .-''-.    .---.     ");
+  printf(
+      "|  _ _   \\     / _     \\               .'_ _   \\\\          \\|   |  "
+      "|_ _|  .'  __ `. |    \\  |  |\\ .-.')  .'_ _   \\   | ,_|     ");
+  printf(
+      "| ( ' )  |    (`' )/`--'              / ( ` )   '`--.  ,---'|   |  ( ' "
+      ") /   '  \\  \\|  ,  \\ |  |/ `-' \\ / ( ` )   ',-./  )     ");
+  printf(
+      "|(_ o _) /   (_ o _).                . (_ o _)  |   |   \\   |   "
+      "'-(_{;}_)|___|  /  ||  |\\_ \\|  | `-'`\"`. (_ o _)  |\\  '_ '`)    ");
+  printf("| (_,_).' __  (_,_). '.              |  (_,_)___|   :_ _:   |      "
+         "(_,_)    _.-`   ||  _( )_\\  | .---. |  (_,_)___| > (_)  )    ");
+  printf("|  |\\ \\  |  |.---.  \\  : _ _         '  \\   .---.   (_I_)   | _ "
+         "_--.   | .'   _    || (_ o _)  | |   | '  \\   .---.(  .  .-'    ");
+  printf(
+      "|  | \\ `'   /\\    `-'  |( ` )         \\  `-'    /  (_(=)_)  |( ' ) | "
+      "  | |  _( )_  ||  (_,_)\\  | |   |  \\  `-'    / `-'`-'|___ ");
+  printf(
+      "|  |  \\    /  \\       /(_{;}_)         \\       /    (_I_)   (_{;}_)| "
+      "  | \\ (_ o _) /|  |    |  | |   |   \\       /   |        \\");
+  printf("''-'   `'-'    `-...-'  (_,_)           `'-..-'     '---'   '(_,_) "
+         "'---'  '.(_,_).' '--'    '--' '---'    `'-..-'    `--------`");
 }
